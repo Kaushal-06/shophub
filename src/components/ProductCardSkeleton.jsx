86f1fa91142244b81866f8e9376f66f9efda11ac
unknown <ktanwar242@gmail.com>
2025-11-15 17:37:59 +0530

    ProductCardSkeleton.jsx is updated

--- src/components/ProductCardSkeleton.jsx
+++ src/components/ProductCardSkeleton.jsx
@@ -7,7 +7,7 @@ const ProductCardSkeleton = () => {
         <div className="h-4 bg-gray-200 rounded w-1/2"></div>
         <div className="flex items-center justify-between mb-2 sm:mb-4 mt-1 sm:mt-2"></div>
         <div className="h-5 sm:h-8 bg-gray-200 rounded w-25 sm:w-20"></div>
-        <div className="h-7 sm:h-10 bg-gray-200 rounded w-50 sm:w-32"></div>
+        <div className="h-7 sm:h-10 bg-gray-200 rounded w-40 sm:w-32"></div>
       </div>
     </div>
   );
